900f91ceaff473b423cc8dea5252dba28d08f56e
DEIVA SIVAM <115927155+asdeiva@users.noreply.github.com>
2023-04-23 17:18:13 +0000

    project started

---
+++ src/components/Common/Header/drawer.js
@@ -0,0 +1,32 @@
+import Drawer from "@mui/material/Drawer";
+import { IconButton } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useState } from "react";
+
+export default function TemporaryDrawer() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div>
+      <IconButton onClick={() => setOpen(true)}>
+        <MenuIcon className="link" />
+      </IconButton>
+      <Drawer anchor={"right"} open={open} onClose={() => setOpen(false)}>
+        <div className="drawer-div">
+          <a href="/">
+            <p className="link">Home</p>
+          </a>
+          <a href="/">
+            <p className="link">Compare</p>
+          </a>
+          <a href="/">
+            <p className="link">Watchlist</p>
+          </a>
+          <a href="/">
+            <p className="link">Dashboard</p>
+          </a>
+        </div>
+      </Drawer>
+    </div>
+  );
+}
